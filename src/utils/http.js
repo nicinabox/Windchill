65a81891d9f26f677a20ba9691ba8bbde1737cc5
Nic Aitch <nic@nicinabox.com>
2016-11-12 11:55:59 -0600

    Add http status check

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,15 @@
 const toJSON = r => r.json()
 
+const checkStatus = (r) => {
+  if (r.ok) {
+    return r
+  }
+
+  let error = new Error(r.statusText)
+  error.response = r
+  throw error
+}
+
 export const get = (url, options = {}) => {
-  return fetch(url, options).then(toJSON)
+  return fetch(url, options).then(checkStatus).then(toJSON)
 }
