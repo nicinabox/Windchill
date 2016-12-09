cc1f422e7416ff1658a9027e4b90ebb20107b0e4
Nic Aitch <nic@nicinabox.com>
2016-12-09 08:27:01 -0600

    Rename FeelsLike -> Windchill

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native'
 import Root from './src/Root'
 
-AppRegistry.registerComponent('FeelsLike', () => Root);
+AppRegistry.registerComponent('Windchill', () => Root);
--- ios/WindchillTests/WindchillTests.m
+++ ios/WindchillTests/WindchillTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface FeelsLikeTests : XCTestCase
+@interface WindchillTests : XCTestCase
 
 @end
 
-@implementation FeelsLikeTests
+@implementation WindchillTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
