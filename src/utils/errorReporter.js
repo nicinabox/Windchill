38ac17d777cdbec54e57dbaa858881934c8eb8c2
Nic Aitch <nic@nicinabox.com>
2016-12-10 12:51:57 -0600

    Fix error reporter swallowing dev bugs

--- src/utils/errorReporter.js
+++ src/utils/errorReporter.js
@@ -1,2 +1,11 @@
 import { Client } from 'bugsnag-react-native'
-export default new Client(process.env.BUGSNAG_API_KEY)
+
+const mockReporter = {
+  notify(err) {
+    console.log(err)
+  }
+}
+
+const client = __DEV__ ? mockReporter : new Client(process.env.BUGSNAG_API_KEY)
+
+export default client
