ba460b85da1aeb1d8e3ddee32a01fb034935d16a
Nic Aitch <nic@nicinabox.com>
2017-05-05 17:29:44 -0500

    Rename feelslike to windchill

--- android/app/src/main/java/com/windchill/MainApplication.java
+++ android/app/src/main/java/com/windchill/MainApplication.java
@@ -1,4 +1,4 @@
-package com.feelslike;
+package com.windchill;
 
 import android.app.Application;
 import android.util.Log;
--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native'
 import Root from './src/Root'
 
-AppRegistry.registerComponent('Windchill', () => Root);
+AppRegistry.registerComponent('Windchill', () => Root)
