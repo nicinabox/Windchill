01288a677da0c3c40c048fb74a9de6970b71f521
Nic Aitch <nic@nicinabox.com>
2017-05-06 13:50:59 -0500

    Add icons, iap utils

--- android/app/src/main/java/com/windchill/MainApplication.java
+++ android/app/src/main/java/com/windchill/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.rctrevmob.RevMobPackage;
 import com.bugsnag.BugsnagReactNative;
 import io.fixd.rctlocale.RCTLocalePackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RevMobPackage(),
             BugsnagReactNative.getPackage(),
             new RCTLocalePackage()
