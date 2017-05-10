94f7651ff4f46bf95e1f33abbc86af8f6230495c
Nic Aitch <nic@nicinabox.com>
2017-05-09 19:55:56 -0500

    Remove logging

--- src/middleware/cache.js
+++ src/middleware/cache.js
@@ -2,7 +2,6 @@ import { setItem } from '../utils/storage'
 
 export default store => next => action => {
   let result = next(action)
-  console.log(store.getState());
   setItem('store', store.getState() || {})
   return result
 }
