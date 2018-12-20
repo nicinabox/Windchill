6195bacd543604a01a693dd50cf4111f31d64d04
Nic Haynes <nic@nicinabox.com>
2018-12-19 20:31:16 -0600

    Load SI before CA

--- src/utils/getLocaleUnits.js
+++ src/utils/getLocaleUnits.js
@@ -4,16 +4,16 @@ import { US, SI, UK, CA } from './conversions'
 const constants = Locale.constants()
 
 export default function getLocaleUnits () {
-  if (constants.countryCode === 'CA') {
-    return CA
-  }
-
   if (constants.countryCode === 'GB') {
     return UK
   }
 
   if (constants.measurementSystem === 'Metric') {
     return SI
+  }
+
+  if (constants.countryCode === 'CA') {
+    return CA
   }
 
   return US
