090066bd5d75416930b215ea02c2d069db821da2
Nic Haynes <nic@nicinabox.com>
2019-11-27 23:22:18 -0600

    Fix handling formula min speed

--- src/utils/windchill.ts
+++ src/utils/windchill.ts
@@ -2,15 +2,17 @@ import { windchill } from 'weather-tools'
 import convert from 'src/utils/convert'
 import { Units, defaultMeasurements } from './units'
 
+// Unit provides UI min, but we want formula min
+const FORMULA_MIN_SPEED = 3
+
 export default (temperature: number, speed: number, units: Units): number => {
-  const minSpeed = units.speed.bounds.min
-  const realSpeed = speed <= minSpeed ? minSpeed : speed
+  const realSpeed = Math.max(FORMULA_MIN_SPEED, speed)
 
   const tempF = convert(temperature, units.temperature, defaultMeasurements.temperature)
   const speedMPH = convert(realSpeed, units.speed, defaultMeasurements.speed)
 
-  const usWindchill = windchill.us(tempF, speedMPH, false)
-  const convertedWindchill = convert(usWindchill, defaultMeasurements.temperature, units.temperature)
+  const windchillUS = windchill.us(tempF, speedMPH, false)
+  const convertedWindchill = convert(windchillUS, defaultMeasurements.temperature, units.temperature)
 
   return convertedWindchill
 }
