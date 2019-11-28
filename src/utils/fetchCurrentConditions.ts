8d2fb7692b943e3e978b080723ebafbae84e23b4
Nic Haynes <nic@nicinabox.com>
2019-11-27 20:58:48 -0600

    Fix env vars

--- src/utils/fetchCurrentConditions.ts
+++ src/utils/fetchCurrentConditions.ts
@@ -1,11 +1,12 @@
-import { get } from 'src/utils/http'
-import { DarkSkyConditionsCurrently, Coordinates } from '../interfaces/api'
-
 // @ts-ignore
-const FORECAST_API_KEY = process.env.FORECAST_API_KEY
+import { FORECAST_API_KEY } from 'react-native-dotenv'
+
+import { get } from 'src/utils/http'
+import { DarkSkyConditionsCurrently, Coordinates } from 'src/interfaces/api'
+
 
 export const createDarkSkyUrl = ({ latitude, longitude }: Coordinates) => {
-  return `https://api.forecast.io/forecast/${FORECAST_API_KEY}/${[latitude, longitude].join(',')}`
+  return `https://api.forecast.io/forecast/${FORECAST_API_KEY}/${latitude},${longitude}`
 }
 
 export default async (coords: Coordinates): Promise<DarkSkyConditionsCurrently> => {
