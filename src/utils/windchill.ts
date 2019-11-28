37691367600d0ea8855972cd1b1c6ac68e7fedff
Nic Haynes <nic@nicinabox.com>
2019-11-28 00:19:56 -0600

    Change windchill to render temp when less than min speed

--- src/utils/windchill.ts
+++ src/utils/windchill.ts
@@ -6,10 +6,12 @@ import { Units, defaultMeasurements } from './units'
 const FORMULA_MIN_SPEED = 3
 
 export default (temperature: number, speed: number, units: Units): number => {
-  const realSpeed = Math.max(FORMULA_MIN_SPEED, speed)
+  if (speed < FORMULA_MIN_SPEED) {
+      return temperature
+  }
 
   const tempF = convert(temperature, units.temperature, defaultMeasurements.temperature)
-  const speedMPH = convert(realSpeed, units.speed, defaultMeasurements.speed)
+  const speedMPH = convert(speed, units.speed, defaultMeasurements.speed)
 
   const windchillUS = windchill.us(tempF, speedMPH, false)
   const convertedWindchill = convert(windchillUS, defaultMeasurements.temperature, units.temperature)
