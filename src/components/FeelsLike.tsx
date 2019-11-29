fbfdd505c20428de9708fd92e779302cd4896a34
Nic Haynes <nic@nicinabox.com>
2019-11-29 12:05:36 -0600

    Restore Feels like text

--- src/components/FeelsLike.tsx
+++ src/components/FeelsLike.tsx
@@ -33,7 +33,7 @@ export const FeelsLike: React.FC<FeelsLikeProps> = ({ value }) => {
           styles.label,
           scaleText({ fontSize: 21 })
         ]}>
-        Feels
+        Feels like
       </Text>
 
       <Text allowFontScaling={false}
